31df46a15dbb5e73b0b2f037b9b8a9618a079923
Peter van Hardenberg <pvh@pvh.ca>
2022-05-27 00:56:20 -0700

    add the start of automerge-repo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
+import { BrowserRepo } from 'automerge-repo'
+const repo = BrowserRepo()
+const handle = repo.create()
+handle.change( (d) => {
+  d.message = "Hello world."
+})
 
 function App() {
+  const [doc, setDoc] = useState({})
+  useEffect(() => {
+    handle.value().then((v) => setDoc(v))
+  })
+
+  const message = doc.message
+  console.log('message', doc, message)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          { message }
         </p>
         <a
           className="App-link"
