dab0bab49e1a11cf204262f96bc3dfc5f33e55cf
Peter van Hardenberg <pvh@pvh.ca>
2022-06-17 09:00:39 -0700

    remove decorations

--- src/App.js
+++ src/App.js
@@ -1,32 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import { useDocument } from './hooks'
 import { useHandle } from './hooks'
 import { Editor } from './prosemirror/Editor';
-import { TodoList } from './components/TodoList';
 
 function App({ rootDocumentId }) {
   const [doc, changeDoc] = useDocument(rootDocumentId)
   const [handle] = useHandle(rootDocumentId)
-  const bumpCounter = (ev) => {
-    changeDoc((doc) => {
-      doc.count = doc.count+1
-    })
-  }
   
   if (!doc) return
-  
-  const { message, count } = doc
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          { message }
-        </p>
-        <TodoList documentId={rootDocumentId}/>
-        <Editor doc={doc} changeDoc={changeDoc} handle={handle} attribute={'message'}/>
-        <button onClick={bumpCounter}>We smashed it {count} times!</button>
+        <Editor doc={doc} changeDoc={changeDoc} handle={handle} attribute={'text'}/>
       </header>
     </div>
   );
